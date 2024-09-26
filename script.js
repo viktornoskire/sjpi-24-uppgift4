4cc4d277d2826d61c11548b566f16af0afb8bf82
ViktorNoskire <vikkerinho@gmail.com>
2024-09-26 11:39:01 +0200

    Commented in my JS file

--- script.js
+++ script.js
@@ -14,40 +14,48 @@ const counter = document.querySelector('#countToDo')
 const infoText = document.querySelector('#infoText')
 
 
+// Declare a function to change status of objects in todoArray
 function changeStatus(text, status) {
     let changeIndex = todoArray.map(t => t.name).indexOf(text);
     todoArray[changeIndex].completed = status;
     console.log(todoArray);
 }
 
+// Declare a function to check if the written input exists already in the todoArray and returns a bool
 function searchExistingElement(input) {
     let searchArray = todoArray.map(t => t.name).includes(input);
     return searchArray;
 }
 
+// Declare a function to serach for the index of the clicked item to then use the index to remove object from todoArray
 function searchIndex(removedItem) {
     let index = todoArray.map(t => t.name).indexOf(removedItem);
     return index;
 }
 
+// The event listener on the addBtn
 addBtn.addEventListener(
     "click",
     function () {
-        let text = myInput.value;
+        const text = myInput.value; // declaring a variable with the value of myInput
 
+        // test if the user have written anything
         if (text == "") {
             infoText.innerHTML = "You have to write something!";
         }
         else {
+
+            // testing if the text that the user wrote already is in the list
             if (searchExistingElement(text)) {
                 infoText.innerHTML = "Task already in your todo-list...";
             }
             else {
-                // 
                 infoText.innerHTML = "";
-                const listItem = document.createElement('li');
-                taskList.appendChild(listItem);
 
+                // Declaring a new li-element and appending it to the unorderd list
+                const listItem = document.createElement('li'); 
+                taskList.appendChild(listItem);
+                // Declaring a span and the textContent is the text-variable, then appending that to the li-element
                 const itemLabel = document.createElement('span');
                 itemLabel.textContent = text;
                 listItem.appendChild(itemLabel);
@@ -63,6 +71,8 @@ addBtn.addEventListener(
                     "click",
                     function () {
                         taskList.removeChild(listItem);
+                        // if completed count is more than 0 then the counter goes down. 
+                        // then using my searchIndex-function to remove object in todoArray
                         if (completedCount > 0) {
                             completedCount--;
                             let removeItem = searchIndex(text);
@@ -92,6 +102,7 @@ addBtn.addEventListener(
 
                 })
 
+                // declare an empty object and push it to the todoArray 
                 const todoObject = {};
                 todoObject.name = text;
                 todoObject.completed = false;
